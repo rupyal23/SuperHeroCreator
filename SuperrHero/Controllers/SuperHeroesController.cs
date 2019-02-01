d51aa4ef4ea4382b76ae23bc01965e41fe498043
Kanwarjit Rupyal <kanwar.rupyal@gmail.com>
2019-02-01 14:51:47 -0600

    Delete logic written too, CRUD Functionality complete

--- SuperrHero/Controllers/SuperHeroesController.cs
+++ SuperrHero/Controllers/SuperHeroesController.cs
@@ -78,17 +78,19 @@ namespace SuperrHero.Controllers
         // GET: SuperHeroes/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            return View(db.SuperHeroes.Find(id));
         }
 
         // POST: SuperHeroes/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(int id, SuperHero Hero)
         {
             try
             {
                 // TODO: Add delete logic here
-
+                var superHeroFromDb = db.SuperHeroes.Find(id);
+                db.SuperHeroes.Remove(superHeroFromDb);
+                db.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
