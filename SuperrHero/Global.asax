193ea6793e2b3b964fa71ba54428ad65d291e5c0
Kanwarjit Rupyal <kanwar.rupyal@gmail.com>
2019-02-01 09:41:43 -0600

    Initial Commit - Made SuperHero Model

---
+++ SuperrHero/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SuperrHero.MvcApplication" Language="C#" %>
---
+++ SuperrHero/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SuperrHero.Startup))]
+namespace SuperrHero
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
